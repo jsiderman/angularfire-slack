b4acfd23f52ef2f311c3e47d5184067282a92f88
Jonathan Siderman <jonsiderman@gmail.com>
2017-03-18 22:37:25 -0700

    Revert "Adding in Firebase auth"
    This reverts commit 0a004bc0d8e4ec3c57116984cf243ef1f78e0e58.

--- app/app.js
+++ app/app.js
@@ -17,30 +17,18 @@ angular
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
-          url: '/',
-          templateUrl: 'home/home.html'
+        url: '/',
+        templateUrl: 'home/home.html'
       })
       .state('login', {
-          url: '/login',
-          controller: 'AuthCtrl as authCtrl',
-          templateUrl: 'auth/login.html'
+        url: '/login',
+        templateUrl: 'auth/login.html'
       })
       .state('register', {
-          url: '/register',
-          controller: 'AuthCtrl as authCtrl',
-          templateUrl: 'auth/register.html'
+        url: '/register',
+        templateUrl: 'auth/register.html'
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/')
-    .config(function(){
-        var config = {
-            apiKey: "AIzaSyA23HwEYvHa39TkSfwEsGj5OUbKBGd5J6s",
-            authDomain: "flack-9fe90.firebaseapp.com",
-            databaseURL: "https://flack-9fe90.firebaseio.com",
-            storageBucket: "flack-9fe90.appspot.com",
-            messagingSenderId: "134057867614",
-        };
-        firebase.initializeApp(config);
-    });
+  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
